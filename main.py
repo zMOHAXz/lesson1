7c4979dc9810c209d46472eb474c8b5e72fc35ba
zMOHAXz <vlastorz@gmail.com>
2024-06-14 16:05:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+print(len('Heiio, world'))
+first = 10
+second = 5
+summa = first + second
+diff = first - second
+print(summa) , print(diff)
+first = 10
+second = 5
+third = 3
+mean = (first + second + third) / 3
+print(mean)
+first_string = 'Понедельник'
+second_string = 'Вторник'
+print(first_string + ', ' + second_string)
+a = 2
+b = 3
+c = 4
+f = ((((a * b) + (a * c)) ** 3) / 2)
+print(f)
+
+
+
