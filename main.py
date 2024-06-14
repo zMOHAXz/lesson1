6fdd148432978894119ba7530edf21bb877a31ec
zMOHAXz <vlastorz@gmail.com>
2024-06-14 16:07:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 print(len('Heiio, world'))
+
 first = 10
 second = 5
 summa = first + second
 diff = first - second
 print(summa) , print(diff)
+
 first = 10
 second = 5
 third = 3
 mean = (first + second + third) / 3
 print(mean)
+
 first_string = 'Понедельник'
 second_string = 'Вторник'
 print(first_string + ', ' + second_string)
+ё
 a = 2
 b = 3
 c = 4
